1f5736998ab3be32548e68264f13ff09e800b4b2
Jade Evelyn Oliveira da Silva <oliveirajade06@gmail.com>
2025-05-05 20:00:34 -0300

    hist

--- livros.py
+++ livros.py
@@ -18,5 +18,11 @@ price_min= df_top100_books["book price"].min()
 max_price = st.sidebar.slider("Price Range", price_min, price_max, price_max)
 #"liga" o slider aos dados
 df_books = df_top100_books[df_top100_books["book price"] <= max_price]
-
+df_books
+#count valores de quantas aparições de cada ano e monta o histograma 
+fig= px.bar(df_top100_books["year of publication"].value_counts())
+fig2 = px.histogram(df_books["book price"])
+col1, col2 = st.columns(2)
+col1.plotly_chart(fig)
+col2.plotly_chart(fig2)
 
